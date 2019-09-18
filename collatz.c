bdb5b5261a2ab0ff6888f752729c1e1740bfc571
maxi512 <maximontenegro512@gmail.com>
2019-09-18 18:48:26 -0300

    realize metodo para el primer ejercicio

--- collatz.c
+++ collatz.c
@@ -2,41 +2,73 @@
 #include <stdlib.h>
 #include <string.h>
 
-int * collatz(int numero, int * size){
-    int i  = 0;
-     
-    //MAXIMO 50 enteros
-    int * resultado  = malloc(50 * sizeof(int));
+/**
+ * *Recibe un numero y devuelve la conjetura de collatz asociada a ese numero en un arreglo
+ * */
+void collatz(int numero, int *size, int **parreglo)
+{
+    int i = 0;
 
-    if(numero == 1){
-       
-        *(resultado + 0) = 1;
-        *(resultado + 1 )= 4;
-        *(resultado + 2) = 2;
-        *(resultado + 3) = 1;
-        (*size) = 4;  
+    int *resultado = (*parreglo);
+
+    resultado[i++] = numero;
+    (*size)++;
 
 
+    if (numero == 1)
+    {
+        resultado[i++] = 4;
+        resultado[i++] = 2;
+        resultado[i++] = 1;
+        (*size) = 4;
     }
-    return resultado;
+
+    else
+    {
+        while (numero != 1)
+        {
+            if (numero % 2 == 0)
+            {
+                //*Es par
+                numero /= 2;
+                resultado[i++] = numero;
+            }
+
+            else
+            {
+                //* Es Impar
+                numero = 3 * numero + 1;
+                resultado[i++] = numero;
+            }
+
+            (*size)++;
+        }
+    }
 
 }
 
-int main(){
-    int size;
-    int * arreglo = collatz(1, &size);
+int main(int argc, char ** args)
+{   
+
+    //!FALTAN HACER CHEQUEOS
+
+    //*Convierte un string a entero
+    int numero = atoi(*(args+1));
+
+    int size = 0;
+
+    //Maximo 500 enteros
+    int *arreglo = (int *) malloc(500 * sizeof(int));
+    collatz(numero, &size, &arreglo);
 
     int i;
-    printf("EL SIZE ES: %d\n",size);
-    for(i = 0; i<size; i++){     
+    printf("EL SIZE ES: %d\n", size);
+    for (i = 0; i < size; i++)
+    {
 
-        printf("%d\n",*arreglo);
-        
-        arreglo++;
-        
+        printf("%d ", arreglo[i]);
     }
-
-    //PREGUNTAR
+    printf("\n");
     free(arreglo);
 
     return 0;
