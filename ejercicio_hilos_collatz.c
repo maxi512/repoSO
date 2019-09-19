d7c4f33ef4e674f4874035a9cc61deb09859b059
maxi512 <maximontenegro512@gmail.com>
2019-09-19 20:17:09 -0300

    realize el ejercicio de hilos (falta testear)

--- ejercicio_hilos_collatz.c
+++ ejercicio_hilos_collatz.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <locale.h>
+#include <pthread.h>
+
+struct parametros
+{
+    int numero;
+    int * size;
+    int **parreglo;
+};
 
 /**
  * *Recibe un numero y devuelve la conjetura de collatz asociada a ese numero en un arreglo
- * */
-void collatz(int numero, int *size, int **parreglo)
+ * 
+ */
+void * collatz(void * arg)
 {
+
+    struct parametros * param = (struct parametros *)arg;
+
+    int numero = param->numero;
+    int * size = param->size;
+    int ** parreglo = param->parreglo;
+    printf("Recibi los parametros!\n");
+
     int i = 0;
 
     int *resultado = (*parreglo);
@@ -31,47 +46,58 @@ void collatz(int numero, int *size, int **parreglo)
             {
                 //*Es par
                 numero /= 2;
-                resultado[i++] = numero;
             }
-
             else
             {
                 //* Es Impar
-                numero = 3 * numero + 1;
-                resultado[i++] = numero;
+                numero = 3 * numero + 1; 
             }
-
+            resultado[i++] = numero;
             (*size)++;
         }
     }
+
+    pthread_exit((void *)resultado);
+
 }
 
 int main(int argc, char **args)
 {
-    if (argc > 2)
+    if (argc > 2 || argc == 1)
     {
-        printf("Por favor ingresar solo un parametro");
+        printf("Por favor ingresar un parametro\n");
     }
     else
     {
         //*Convierte un string a entero
         int numero = atoi(*(args + 1));
-
-        //atoi devuelve 0 si no pudo realizar la conversion.
-        if (numero == 0 || numero < 0)
+        //! atoi devuelve 0 si no pudo realizar la conversion.
+        if (numero <= 0)
         {
             printf("Por favor ingrese como parametro un numero entero positivo.\n");
         }
         else
         {
+            pthread_t hilo_hijo;
 
             int size = 0;
 
             //Maximo 500 enteros
             int *arreglo = (int *)malloc(500 * sizeof(int));
-            collatz(numero, &size, &arreglo);
+
+            //creo struct para parametrizar
+            struct parametros * param = malloc(sizeof(struct parametros));
+
+            param->numero = numero;
+            param->parreglo = &arreglo;
+            param->size = &size;
+
+            pthread_create(&hilo_hijo, NULL, &collatz,(void *)param);
+
+            pthread_join(hilo_hijo,(void *)&arreglo);
 
             int i;
+
             printf("EL SIZE ES: %d\n", size);
             for (i = 0; i < size; i++)
             {
@@ -80,6 +106,7 @@ int main(int argc, char **args)
             }
             printf("\n");
             free(arreglo);
+            free(param);
         }
     }
 
