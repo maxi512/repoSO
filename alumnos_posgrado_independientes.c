7832b2478caa72bb0d60d445b798ca7acfa6d3b6
joaquin <joaquinrau@yahoo.com.ar>
2019-11-12 15:43:40 -0300

    Ejercicio alumnos

---
+++ alumnos_posgrado_independientes.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+void *alumnoA(void * arg){
+    int tiempo= 3;
+    for(int i=1; i<4; i++){
+        printf("Soy el alumno A, voy a cursar el curso [%d]\n",i);
+        sleep(tiempo);
+        printf("Soy A, termine de cursar [%d]\n", i);
+    }
+}
+
+void *alumnoB(void * arg){
+    int tiempo= 5;
+    for(int i=1; i<4; i++){
+        printf("Soy el alumno B, voy a cursar el curso [%d]\n",i);
+        sleep(tiempo);
+        printf("Soy B, termine de cursar [%d]\n", i);
+    }
+}
+
+void *alumnoC(void * arg){
+    int tiempo= 2;
+    for(int i=1; i<4; i++){
+        printf("Soy el alumno C, voy a cursar el curso [%d]\n",i);
+        sleep(tiempo);
+        printf("Soy C, termine de cursar [%d]\n", i);
+    }
+}
+
+void *alumnoD(void * arg){
+    int tiempo= 3;
+    for(int i=1; i<4; i++){
+        printf("Soy el alumno D, voy a cursar el curso [%d]\n",i);
+        sleep(tiempo);
+        printf("Soy D, termine de cursar [%d]\n", i);
+    }
+}
+
+void *alumnoE(void * arg){
+    int tiempo= 4;
+    for(int i=1; i<4; i++){
+      printf("Soy el alumno E, voy a cursar el curso [%d]\n",i);
+        sleep(tiempo);
+        printf("Soy E, termine de cursar [%d]\n", i);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+    pthread_t thread_al1,thread_al2,thread_al3,thread_al4,thread_al5;
+
+    pthread_create(&thread_al1, NULL, &alumnoA, NULL);
+    pthread_create(&thread_al3, NULL, &alumnoB, NULL);
+    pthread_create(&thread_al2, NULL, &alumnoC, NULL);
+    pthread_create(&thread_al4, NULL, &alumnoD, NULL);
+    pthread_create(&thread_al5, NULL, &alumnoE, NULL);
+
+
+    pthread_join(thread_al1, NULL);
+    pthread_join(thread_al2, NULL);
+    pthread_join(thread_al3, NULL);
+    pthread_join(thread_al4, NULL);
+    pthread_join(thread_al5, NULL);
+
+
+    return 0;
+}
